f3e5987d8a2ffc83c826fc69be269828a8923cf2
Alex Berman <alex@timebend.net>
2022-11-16 13:57:33 +0100

    WIP: Add positions without content

--- engine.js
+++ engine.js
@@ -22,24 +22,33 @@ const nlusInfo = [
 ];
 
 const maxItemsPerRow = 6;
-const numRows = 3;
-const itemWidth = 200;
-const itemHeight = 200;
+const numRows = 6;
+const numRowsWithContent = 3;
+const offsetForRowsWithContent = 2;
 const horizontalMargin = 10;
 
+var numColumns;
+var itemWidth;
+var itemHeight;
 var displayedItems = [];
 var pageWidth;
 var pageHeight;
 var globalTop;
 var globalLeft;
-var availablePositions;
+var availablePositionsWithoutContent = new Set();
+var availablePositionsWithContent = new Set();
 
 function start() {
     pageWidth = document.body.clientWidth;
     pageHeight = document.body.clientHeight;
-    globalTop = (pageHeight - numRows * itemHeight) / 2;
-    globalLeft = (pageWidth - maxItemsPerRow * itemWidth) / 2;
-    availablePositions = new Set([...Array(nlusInfo.length).keys()]);
+    itemHeight = pageHeight / numRows;
+    itemWidth = itemHeight;
+    numColumns = Math.floor(pageWidth / pageHeight * numRows);
+    offsetForColumnsWithContent = Math.floor((numColumns - maxItemsPerRow) / 2);
+    globalTop = -itemHeight / 2;
+    globalLeft = 0;
+
+    createAvailablePositions();
 
     document.addEventListener("keydown", function(e) {
         e = e || window.event;
@@ -47,6 +56,29 @@ function start() {
             proceed();
         }
     });
+}
+
+function createAvailablePositions() {
+    for(let row = 0; row < numRows; row++) {
+        for(let column = 0; column < numColumns; column++) {
+            var position = {row: row, column: column};
+            if(positionHasContent(row, column)) {
+                availablePositionsWithContent.add(position);
+            }
+            else {
+                availablePositionsWithoutContent.add(position);
+            }
+        }
+    }
+}
+
+function positionHasContent(row, column) {
+    if(row >= offsetForRowsWithContent && row < (offsetForRowsWithContent + numRowsWithContent)) {
+        var columnsInRow = maxItemsPerRow - (1 - row % 2);
+        if(column >= offsetForColumnsWithContent && column < (offsetForColumnsWithContent + columnsInRow)) {
+            return true;
+        }
+    }
 }
 
 function proceed() {
@@ -68,30 +100,20 @@ function createItem(nlu) {
     content.className = "center";
     content.innerHTML = nlu.name;
     item.appendChild(content);
-    var positionIndex = consumeAvailablePosition();
-    placeItem(item, positionIndex);
+    var position = consumeRandom(availablePositionsWithContent);
+    placeItem(item, position.row, position.column);
     return item;
 }
 
-function consumeAvailablePosition() {
-    var availablePositionsAsList = [...availablePositions.values()];
-    var position = availablePositionsAsList[Math.floor(Math.random() * availablePositionsAsList.length)];
-    availablePositions.delete(position);
-    return position;
+function consumeRandom(set) {
+    var asList = [...set.values()];
+    var randomValue = asList[Math.floor(Math.random() * asList.length)];
+    set.delete(randomValue);
+    return randomValue;
 }
 
-function placeItem(item, positionIndex) {
-    var row;
-    var count = 0;
-    for(row = 0; row < numRows; row++) {
-        let numItemsInRow = maxItemsPerRow - (1 - row % 2);
-        if(positionIndex < count + numItemsInRow) {
-            break;
-        }
-        count += numItemsInRow;
-    }
-    const column = positionIndex - count;
+function placeItem(item, row, column) {
     var rowLeft = (1 - row % 2) * itemWidth / 2;
-    item.style.top = globalTop + row * itemHeight;
+    item.style.top = globalTop + (row + offsetForRowsWithContent) * itemHeight;
     item.style.left = globalLeft + rowLeft + column * itemWidth + (column - 1) * horizontalMargin;
 }
