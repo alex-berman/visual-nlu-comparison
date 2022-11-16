2641cdbec318d0dc61c180a4979a54ae07d3a6b4
Alex Berman <alex@timebend.net>
2022-11-16 09:38:30 +0100

    Add NLUs, fix line height problem

--- engine.js
+++ engine.js
@@ -1,11 +1,36 @@
 const nlusInfo = [
+    // Open source
     {name: "Rasa"},
     {name: "Snips"},
+    {name: "ConvLab"},
+    {name: "DeepPavlov"},
+    {name: "Plato", href: "https://eng.uber.com/plato-research-dialogue-system/"},
+
+    // Proprietary
+    {name: "Dialogflow"},
+    {name: "Wit.ai"},
+    {name: "IBM Watson Assistant"},
+    {name: "Amazon Lex"},
+    {name: "LUIS"},
+    {name: "Oracle Digital Assistant"},
+    {name: "SAP Conversational AI"},
+    {name: "Teneo"},
+    {name: "Ada", href: "https://www.ada.cx/platform/conversational-ai"},
+    {name: "Cognigy", href: "https://www.cognigy.com/products/cognigy-ai"},
+    {name: "Ultimate", href: "https://www.cognigy.com/products/cognigy-ai"},
+    {name: "Sprinklr", href: "https://www.sprinklr.com/platform/"},
 ];
 
+const numRows = 3;
+
 var displayedItems = [];
+var pageWidth;
+var pageHeight;
 
 function start() {
+    pageWidth = document.body.clientWidth;
+    pageHeight = document.body.clientHeight;
+
     document.addEventListener("keydown", function(e) {
         e = e || window.event;
         if(e.key == "ArrowRight") {
@@ -15,18 +40,27 @@ function start() {
 }
 
 function proceed() {
-    console.log("proceed");
     if(displayedItems.length < nlusInfo.length) {
         var nluCount = displayedItems.length;
-        var item = createItem(nlusInfo[nluCount]);
+        var nluInfo = nlusInfo[nluCount];
+        var item = createItem(nluInfo, nluCount);
         document.body.appendChild(item);
         displayedItems.push(item);
     }
 }
 
-function createItem(nlu) {
+function createItem(nlu, count) {
     var item = document.createElement("div");
     item.className = "item";
-    item.innerHTML = nlu.name;
+    var content = document.createElement("div");
+    content.className = "center";
+    content.innerHTML = nlu.name;
+    item.appendChild(content);
+    placeItem(item, count);
     return item;
+}
+
+function placeItem(item, count) {
+    item.style.top = pageHeight / 2 - 200/2;
+    item.style.left = pageWidth / 2 - 200/2;
 }
